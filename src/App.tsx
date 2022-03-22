d60e214b4e4fc5f89e2aff28a490b391bb5c102c
Richard Oh <n24512@protonmail.com>
2022-03-22 14:57:02 -0500

    creates newtodo list and todo list components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
+      <NewTodo todoAddHandler={todoAddHandler} />
       <TodoList todos={todos} />
     </div>
   );
